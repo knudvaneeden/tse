93a1a5d8d0b090dc14229e5e6a2adc689d9a67bf
knudvaneeden <knud_van_eeden@yahoo.com>
2018-02-18 06:22:32 +0100

    Next commit: 18/02/2018  6:22:31

--- savefivc.s
+++ savefivc.s
@@ -1,6 +1,6 @@
 cdd C:\TEMP\TSE\
 g:\cygwin\bin\git.exe add .
-g:\cygwin\bin\git.exe commit -m "Next commit: 18/02/2018  6:19:28"
+g:\cygwin\bin\git.exe commit -m "Next commit: 18/02/2018  6:22:10"
 g:\cygwin\bin\git.exe remote add origin https://github.com/knudvaneeden/tse
 g:\cygwin\bin\git.exe remote -v
 keystack "knudvaneeden" enter "knud4github" enter & g:\cygwin\bin\git.exe push origin master
