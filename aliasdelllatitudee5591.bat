ed1d4b467efd1c79e52d59c3a71280dc0d8e6d63
Knud van Eeden <knud_van_eeden@yahoo.com>
2026-03-02 19:46:12 +0100

    [aliasdelllatitudee5591.bat]_recompile

---
+++ aliasdelllatitudee5591.bat
@@ -0,0 +1,17 @@
+@REM version 1.0.0.0.6
+
+@REM [kn, vo, tu, 30-10-2012 13:21:22]-[kn, ri, su, 26-01-2014 01:56:22]
+@REM [kn, ri, su, 18-12-2016 01:24:10]-[kn, ri, su, 18-12-2016 01:37:40]
+@REM [kn, ri, su, 25-12-2016 02:00:03]-[kn, ri, th, 29-12-2016 20:30:01]
+@REM [kn, ri, mo, 02-03-2026 19:43:46]
+
+@REM unalias * & alias /L & alias /R f:\4dos\alias\dell\latitudee5591\alias.dok
+@REM unalias * & alias /L & alias /R /Z f:\4dos\alias\dell\latitudee5591\alias.dok
+@REM unalias * & alias /L & alias /R f:\4dos\alias\dell\latitudee5591\alias.dok
+@REM unalias * & alias /L & alias /R /Z f:\4dos\alias\dell\latitudee5591\alias.dok
+@REM unalias * & alias /L & alias /R /Z f:\4dos\alias\dell\latitudee5591\aliasUnicode.dok
+unalias * & alias /L & alias /R /Z f:\4dos\alias\dell\latitudee5591\alias.dok
+
+copy f:\4dos\alias\ c:\4dos\alias\ /u /s
+
+prompt $p $d $t$g
