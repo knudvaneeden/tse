684b4bcac5ecfeb4ac0449d2a450c04db5f00209
Knud van Eeden <knud_van_eeden@yahoo.com>
2026-03-03 15:10:49 +0100

    [startri.bat]_recompile

---
+++ startri.bat
@@ -0,0 +1,16 @@
+@REM version 1.0.0.0.6
+
+@REM [kn, ri, su, 07-03-2010 15:53:14]-[kn, am, fr, 29-10-2010 21:17:47]
+@REM [kn, am, fr, 29-10-2010 21:18:36]-[kn, am, fr, 05-11-2010 10:49:07]
+@REM [kn, ri, we, 27-03-2013 22:40:41]-[kn, ri, tu, 13-10-2020 18:30:10]
+@REM [kn, ri, tu, 03-03-2026 14:45:48]
+
+SET drive=f:
+
+SET drivetse=c:
+
+@REM call %drive%\startristudio17.bat %drive% %drivetse%
+@REM call %drive%\startridelle6530.bat %drive% %drivetse%
+call %drive%\startridelle5591.bat %drive% %drivetse%
+
+exit
