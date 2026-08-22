c621e86d92abc636df85d7974c2141a0919dc8a3
Knud van Eeden <knud_van_eeden@yahoo.com>
2026-08-22 20:58:50 +0200

    Update viewfinds directory files

--- VIEWFINDS/dddviewfindsgit.bat
+++
@@ -1,9 +0,0 @@
-@REM version: 1.0.0.0.4 [kn, ri, fr, 21-08-2026 00:02:49] [kn, ri, fr, 21-08-2026 03:40:49]-[kn, ri, fr, 21-08-2026 13:05:29]-[kn, ri, fr, 21-08-2026 13:07:20]-[kn, ri, sa, 22-08-2026 18:03:54]
-
-@echo off
-cdd g:\versioncontrol\git\ddd01\viewfinds\
-git add ViewFinds.s
-git add ViewFinds_readme.md
-git commit -m "Update viewfinds directory files"
-git push origin HEAD:TRUNK
-start https://github.com/knudvaneeden/tse/tree/TRUNK/VIEWFINDS
