d83360ecc8890040879e148fd52d05a08cedbfb1
Knud van Eeden <knud_van_eeden@yahoo.com>
2026-09-07 15:10:55 +0200

    Update getfisme.zip directory files

--- GETFISME/getfisme.s
+++ GETFISME/getfisme.s
@@ -1,22 +1,22 @@
 FORWARD INTEGER PROC FNBlockGetBufferMemorySizeCurrentI()
-FORWARD INTEGER PROC FNFileGetBufferMemorySizeCurrentI()
+FORWARD INTEGER PROC FNBufferGetMemorySizeCurrentI()
 FORWARD PROC Main()
 
 
 // --- MAIN --- //
 
 PROC Main()
- Warn( "Total bytes used in this buffer", " ", "=", " ", FNFileGetBufferMemorySizeCurrentI() ) // gives e.g. 12345 bytes as filesize on memory
+ Warn( "Total bytes used in this buffer", " ", "=", " ", FNBufferGetMemorySizeCurrentI() ) // gives e.g. 12345 bytes as filesize on memory
 END
 
 <F12> Main()
 
 // --- LIBRARY --- //
 
-// library: file: get: size: buffer: memory: current <description></description> <version control></version control> <version>1.0.0.0.5</version> <version control></version control> (filenamemacro=getfisme\getfisme.s) [<Program>] [<Research>] [kn, ri, sa, 05-09-2026 18:24:30]
-INTEGER PROC FNFileGetBufferMemorySizeCurrentI()
+// library: buffer: get: memory: size: current <description></description> <version control></version control> <version>1.0.0.0.7</version> <version control></version control> (filenamemacro=getfisme\getfisme.s) [<Program>] [<Research>] [kn, ri, sa, 05-09-2026 18:24:30]
+INTEGER PROC FNBufferGetMemorySizeCurrentI()
  // e.g. PROC Main()
- // e.g.  Warn( "Total bytes used in this buffer", " ", "=", " ", FNFileGetBufferMemorySizeCurrentI() ) // gives e.g. 12345 bytes as filesize on memory
+ // e.g.  Warn( "Total bytes used in this buffer", " ", "=", " ", FNBufferGetMemorySizeCurrentI() ) // gives e.g. 12345 bytes as filesize on memory
  // e.g. END
  // e.g.
  // e.g. <F12> Main()
@@ -37,7 +37,7 @@ INTEGER PROC FNFileGetBufferMemorySizeCurrentI()
  //
 END
 
-// library: block: get: size: memory: current <description></description> <version control></version control> <version>1.0.0.0.12</version> <version control></version control> (filenamemacro=getblsme.s) [<Program>] [<Research>] [kn, ri, sa, 05-09-2026 18:29:07]
+// library: block: get: size: memory: current <description></description> <version control></version control> <version>1.0.0.0.15</version> <version control></version control> (filenamemacro=getblsme.s) [<Program>] [<Research>] [kn, ri, sa, 05-09-2026 18:29:07]
 INTEGER PROC FNBlockGetBufferMemorySizeCurrentI()
  // e.g. PROC Main()
  // e.g.  Warn( FNBlockGetBufferMemorySizeCurrentI() ) // gives e.g. 12345 bytes as blocksize in memory
@@ -51,16 +51,28 @@ INTEGER PROC FNBlockGetBufferMemorySizeCurrentI()
  //
  INTEGER blockSizeI = 0
  //
+ INTEGER lineEndCharacterTotalI = 0
+ //
  IF ( NOT ( IsBlockInCurrFile() ) ) Warn( "Please mark a block" ) RETURN( -1 ) ENDIF // return from the current procedure if no block is marked
  //
  PushPosition()
  PushBlock()
  //
+ IF ( ( WhichOS() == _WINDOWS_ ) OR ( WhichOS() == _WINDOWS_NT_ ) )
+  //
+  lineEndCharacterTotalI = 2
+  //
+ ELSEIF ( WhichOS() == _LINUX_ )
+  //
+  lineEndCharacterTotalI = 1
+  //
+ ENDIF
+ //
  GotoBlockBegin()
  //
  FOR I = minI to maxI
   //
-  blockSizeI = blockSizeI + CurrLineLen() + 2 // always 2 (invisible) characters at the end of the line, ASCII 10 (line feed) and ASCII 13 (carriage return)
+  blockSizeI = blockSizeI + CurrLineLen() + lineEndCharacterTotalI // 1 if Linux or when Microsoft Windows 2 (invisible) characters at the end of the line, ASCII 10 (line feed) and ASCII 13 (carriage return)
   //
   Down()
   //
